e20588e7c6e9bdc2431e9664cfebb6ad4deb2d55
Patrick Reinhart <patrick.reinhart@bison-group.com>
2020-05-08 07:12:10 +0200

    Adds delay for testing parallel installations

--- src/main/java/net/reini/izpack/demo/DemoProcess.java
+++ src/main/java/net/reini/izpack/demo/DemoProcess.java
@@ -3,6 +3,8 @@ package net.reini.izpack.demo;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import com.izforge.izpack.panels.process.AbstractUIProcessHandler;
@@ -15,7 +17,12 @@ public class DemoProcess {
   public boolean run(AbstractUIProcessHandler handler, String[] arguments) {
     Logger log = Logger.getLogger(getClass().getName());
     List<String> argumentList = Arrays.asList(arguments);
-    log.warning(String.format("DemoProcess.run(%s, %s)", handler.toString(), argumentList));
+    log.warning(String.format("DemoProcess.run(%s, %s) with 30 second delay", handler.toString(), argumentList));
+    try {
+      TimeUnit.SECONDS.sleep(30);
+    } catch (InterruptedException e) {
+      log.log(Level.SEVERE, "Unexpected interruption", e);
+    }
     return !argumentList.contains("fail");
   }
 }
